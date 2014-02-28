e11757b9b7fd3b40584520cd67ca3cdbf3a4d29b
miercoledi <github.com@meerkat.cc>
2014-02-28 04:56:49 -0800

    working cl-to-markup generator

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ cl-html-embed
 =============
 
 make lisp code easily-embeddable in html &amp; html-ish file
+
+01. load up process-html-file.lisp
+02. run the function #'htcl-to-html on index.htcl
+03. see the generated file, index.htcl.html (unless otherwise specified)
+
+
+----------------------------------------
+
+basic example of how to embed lisp in your htcl files:
+
+----------------------------------------
+
+<div user="<((sb-posix::getenv "user"))>">
+ <pre id="timestamp">
+  <(
+   (get-universal-time)
+  )>
+ </pre>
+</div>
+
+----------------------------------------
+
+these things, for now, must be made into html/cl files first
+and then converted into pure html, but that can easily be
+done on the fly soon.
--- process-html-file.lisp
+++ process-html-file.lisp
@@ -1,6 +1,36 @@
-
-
 (defun load-html-file (filepath)
   (with-open-file (fstream filepath :direction :input)
     (loop for line = (read-line fstream nil 'eof)
 	 until (eq line 'eof) collect line)))
+
+(defun string-list-to-string (string-list)
+  (eval (append '(concatenate 'string) string-list)))
+
+(defun process-a-string (string)
+  (let ((lhs (search "<(" string))
+	(rhs (search ")>" string)))
+    (cond ((or (null lhs) (null rhs)) string)
+	  (t
+	   (let* ((mid  (subseq string (+ 2 lhs) rhs))
+		  (mid2 (read-from-string mid))
+		  (mid3 (eval mid2))
+		  (mid4 (if (stringp mid3) mid3 (write-to-string mid3))))
+	     (concatenate 'string
+			  (subseq string 0 lhs)
+			  mid4
+			  (process-a-string
+			   (subseq string (+ 2 rhs)))))))))
+
+(defun process-html-file (filepath)
+  (process-a-string
+   (string-list-to-string
+    (load-html-file filepath))))
+
+(defun htcl-to-html (inpath &optional outpath)
+  (let ((processed-data (process-html-file inpath))
+	(outpath (if outpath outpath
+		     (concatenate 'string inpath ".html"))))
+    (with-open-file (fstream outpath :direction :output
+			             :if-does-not-exist :create
+				     :if-exists :rename)
+      (format fstream "~a" processed-data))))
